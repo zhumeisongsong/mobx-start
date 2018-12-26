ca296c75183dfc6246d7d024e1926d62f26b889f
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-12-26 18:26:29 +0900

    Ten minute introduction to MobX and React

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+// Important tips:Test point from dom render data to store data
+test('renders without crashing', () => {
+
 });
+
+
